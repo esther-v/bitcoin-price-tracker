1eb1a3a68889a7a19fe8ccb987c6cb86a10cf541
esther-v <esvalas@gmail.com>
2021-11-05 16:39:58 +0100

    update 20 sec

--- script.js
+++ script.js
@@ -16,9 +16,21 @@ const getPrice = () => {
                 let response = req.response // stocke la réponse
 
                 let dollarsPrice = response.USD.last
+                let eurosPrice = response.EUR.last
+
+                //afficher les données
+                document.querySelector('#price_label').textContent = dollarsPrice
+                document.querySelector('#price_label_eur').textContent = eurosPrice
             }
         }
+        else{
+            alert('un problème est survenu, merci de revenir plus tard')
+        }
     }
-    
-
+    console.log("prix actualisé")
 }
+
+getPrice()
+
+//mise à jour toutes les 20 sec
+setInterval(getPrice, 20000)
